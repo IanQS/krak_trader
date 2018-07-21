b50ddd74c6c6e6581d2048470ef9f292f23326ed
Ian Quah <IanQS@users.noreply.github.com>
2018-07-21 00:03:53 -0400

    Conv Refactor (#7)
    * A method to test efficacy of autoencoder
    
    * updated code. Should not have pushed earlier
    
    * rename

--- src/kraken_brain/utils.py
+++ src/kraken_brain/utils.py
@@ -1,6 +1,28 @@
+"""
+kraken_brain/Utils.py - Utils agnostic to models
+
+Author: Ian Q.
+
+Notes:
+
+"""
+
 import numpy as np
 import os
 from sklearn.preprocessing import minmax_scale, robust_scale, normalize
+import tensorflow as tf
+
+def variable_summaries(var, name):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope('summaries/{}'.format(name)):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
 
 
 def get_image_from_np(data_path: str, currency: str) -> list:
@@ -44,6 +66,17 @@ def clear_tensorboard(path: str):
                 os.unlink(file_path)
         except Exception as e:
             print(e)
+
+def ob_diff(data, final_shape=None, percentagize=False):
+    assert final_shape is not None
+    if isinstance(data, list):
+        data = data[0]
+    diff = np.diff(data, axis=0)
+    if percentagize:
+        return (diff / data[:-1]).reshape(final_shape)
+    return diff.reshape(final_shape)
+
+
 
 def split_data(data: np.array, batch_size: int, maintain_temporal: bool = True) -> tuple:
     """ Splits data into training and validation. Currently only supports
