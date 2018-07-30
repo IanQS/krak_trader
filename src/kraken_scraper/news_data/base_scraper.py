725a217575df6d0934519288095bf549ae56fc8b
Ian <Itq@alumni.cmu.edu>
2018-07-30 06:41:00 -0400

    skeleton of a scraper

--- src/kraken_scraper/news_data/base_scraper.py
+++ src/kraken_scraper/news_data/base_scraper.py
@@ -15,38 +15,46 @@ Notes:
 import os
 from abc import ABC, abstractmethod
 import re as regexp
+from constants import NEWS_PATH
 
 import numpy as np
 
 
 class GenericScraper(ABC):
-    def __init__(self, website: str, site_dir: str, seen_urls: set = None):
-        """ Inherited method
-
-        :param website: website to scrape from
+    def __init__(self, site_dir: str, ):
+        """ Inherited init method
         :param site_dir: directory to save data to
         :param seen_urls: empty dict (new scraper), OR set data loaded in
         """
-        self.url = website
-        self.storage_site = site_dir
-        self.seen_sites = seen_urls if seen_urls else {}
+        self.storage_absolute = NEWS_PATH.format(site_dir)
+        self.seen_sites, self.seen_sites_store = self._storage_site_setup()
         self.parser = regexp.compile('\d{2}\-[a-zA-Z]*\-\d{4}')
 
-    def parse_url(self, url):
-        pass
+    def _storage_site_setup(self):
+        """ Construct the storage site (if not exists), and initiate
+        the 'seen sites' file from which we store URLs
+        """
+        seen_file = '/seen_data.txt'
+        seen_sites_store = storage_absolute + seen_file
+        if not os.path.isdir(storage_absolute):
+            os.mkdir(storage_absolute)
 
-    @abstractmethod
-    def _parse_url(self):
-        raise NotImplementedError
+            # Create the storage file too
+            with open(seen_sites_store, 'a') as f:
+                f.close()
+        with open(seen_sites_store, 'r') as f:
+            data = f.readlines()
+            data = [datum.strip() for datum in data]
+            return set(data), seen_sites_store
 
-    def save_article(self, article, url, date):
-        with open(os.path.join(self.storage_site, 'seen_sites.txt'), 'a') as f:
+    def save_article(self, article, url):
+        with open(self.seen_sites_store, 'a') as f:
             f.write(url)
 
         if not self.parser.match(date):
-            raise ValueError('Data not formatted correctly')
+            raise ValueError('Date not formatted correctly')
 
-        f_name = os.path.join(self.storage_site, url)
+        f_name = os.path.join(self.storage_absolute, url)
         np.savez(f_name, article)
 
     @abstractmethod
--- src/kraken_scraper/news_data/crypto_coins_news.py
+++ src/kraken_scraper/news_data/crypto_coins_news.py
@@ -1,1 +1,22 @@
 from .base_scraper import GenericScraper
+from news_api_key import key
+from newsapi import NewsApiClient
+
+class CryptoCoinNews(GenericScraper):
+    def __init__(self):
+        api = NewsApiClient(api_key=key)
+        super().__init__('/crypto_coins_news', )
+
+    def _save_article(seen_article):
+
+        super().save_article(article, url)
+
+
+    def get_articles():
+        while True:
+
+
+
+if __name__ == '__main__':
+    scraper = CryptoCoinNews()
+    scraper.
