d641352307dfba2e8d531801bd72cd210ad7c384
Ian Q <itq@alumni.cmu.edu>
2018-08-01 00:35:54 -0400

    eh why not

--- src/kraken_scraper/news_data/base_scraper.py
+++ src/kraken_scraper/news_data/base_scraper.py
@@ -27,7 +27,7 @@ class GenericScraper(ABC):
         :param seen_urls: empty dict (new scraper), OR set data loaded in
         """
         self.storage_absolute = NEWS_PATH.format(site_dir)
-        self.seen_sites, self.seen_sites_store = self._storage_site_setup()
+        self.seen_sites, self._seen_sites_store = self._storage_site_setup()
         self.parser = regexp.compile('\d{2}\-[a-zA-Z]*\-\d{4}')
 
     def _storage_site_setup(self):
@@ -35,9 +35,9 @@ class GenericScraper(ABC):
         the 'seen sites' file from which we store URLs
         """
         seen_file = '/seen_data.txt'
-        seen_sites_store = storage_absolute + seen_file
-        if not os.path.isdir(storage_absolute):
-            os.mkdir(storage_absolute)
+        seen_sites_store = self.storage_absolute + seen_file
+        if not os.path.isdir(self.storage_absolute):
+            os.mkdir(self.storage_absolute)
 
             # Create the storage file too
             with open(seen_sites_store, 'a') as f:
@@ -47,16 +47,17 @@ class GenericScraper(ABC):
             data = [datum.strip() for datum in data]
             return set(data), seen_sites_store
 
-    def save_article(self, article, url):
-        with open(self.seen_sites_store, 'a') as f:
+    def save_article(self, article, url:str, date:str, site:str, author: str = None):
+        with open(self._seen_sites_store, 'a') as f:
             f.write(url)
 
         if not self.parser.match(date):
             raise ValueError('Date not formatted correctly')
 
         f_name = os.path.join(self.storage_absolute, url)
-        np.savez(f_name, article)
-
-    @abstractmethod
-    def _save_article(self):
-        raise NotImplementedError
+        data = {"article": article,
+                "url": url,
+                "date": date,
+                "site": site,
+                "author": author}
+        np.savez(f_name, **data)
--- src/kraken_scraper/news_data/crypto_coins_news.py
+++ src/kraken_scraper/news_data/crypto_coins_news.py
@@ -6,27 +6,91 @@ Author: Ian Q.
 Notes:
 
 """
-
+import sys
 
 from .base_scraper import GenericScraper
 from news_api_key import key
 from newsapi import NewsApiClient
+from newsapi.newsapi_exception import NewsAPIException
 
 class CryptoCoinNews(GenericScraper):
-    def __init__(self):
+    def __init__(self, source):
         self.api = NewsApiClient(api_key=key)
-        super().__init__('/crypto_coins_news', )
+        self.source = source
+        super().__init__('/{}'.format(self.source))
         self.get_articles()
 
-    def _save_article(seen_article):
-        super().save_article(article, url)
+    def _process(self, query) -> dict:
+        ################################################
+        #TODO: Bryan: overwrite query[article] with the extracted article from the link query['url']
+        ################################################
+
+        holder = {}
+        for access_key in ['article', 'url', 'date', 'site', 'author']:
+            holder[access_key] = query[access_key]
+        return holder
+
+    def process(self, query_results: list) -> None:
+        """ Process the queries - do any scraping on them
+        before calling self.save_article()
+
+        :param queries:
+        :return:
+            None
+        """
+        for query in query_results:
+            query = self.__substitution(query)
+            processed_data = self._process(query)
+            self.save_article(**processed_data)
+
+    def __substitution(self, query):
+        """ Fixes query to have the fields expected by our saver
+
+        :param query:
+        :return:
+        """
+        query['site'] = query['source']['name']
+        query['date'] = query['publishedAt']
+        return query
 
 
-    def get_articles():
+    def get_articles(self):
+        """ Infinite while-loop that queries the website for any
+        articles it has not seen
+
+        :return:
+        """
+        ind = 1  # NewsAPIClient starts indexing at 1
+        page_size = 100
         while True:
+            queries, ind = self.safe_query(ind, page_size=page_size)
+            if queries is None:
+                page_size=10
+            self.process(queries)
 
-
+    def safe_query(self, ind, page_size=100):
+        error_message = 'Error retrieving from {}. Error: {}'
+        try:
+            if ind is None:  # Only here if we've hit maximumResultsReached before
+                ind = 1
+            q = self.api.get_everything(q='bitcoin',
+                                        sources=self.source,
+                                        language='en',
+                                        page_size=page_size,
+                                        sort_by='publishedAt',
+                                        page=ind)
+        except NewsAPIException as e:
+            ################################################
+            # Reached end of queries. Reset to first page
+            ################################################
+            if e['code'] == 'maximumResultsReached':
+                return None, None
+        except Exception as e:
+            print(error_message.format(self.__class__.__name__, e))
+            sys.exit(0)
+        if q['status'] != 'ok':
+            print(error_message.format(self.__class__.__name__, 'Status: {}'.format(q['status'])))
+        return q, ind + 1
 
 if __name__ == '__main__':
-    scraper = CryptoCoinNews()
-    scraper.
+    scraper = CryptoCoinNews('crypto_coins_news')
